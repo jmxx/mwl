93c9b4ad36027aecf525831a2e62876ef11a8345
Juan Manuel Reynoso <yeah.mue@gmail.com>
2017-06-28 20:19:09 -0500

    Change timezone; Seed user

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Eloquent\Model;
+
+class UsersTableSeeder extends Seeder
+{
+  /**
+   * Run the database seeds.
+   *
+   * @return void
+   */
+  public function run()
+  {
+    DB::table('users')->insert([
+      'name' => 'Juan Manuel Reynoso',
+      'username' => 'jmxx',
+      'email' => 'jmxx@mwl.mx',
+      'password' => bcrypt('pass123'),
+      'created_at' => new DateTime,
+      'updated_at' => new DateTime
+    ]);
+  }
+
+}
