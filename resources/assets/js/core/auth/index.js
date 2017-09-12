64ef822d4daa101eb297b6af0661ca34bc9a7904
Juan Manuel Reynoso <yeah.mue@gmail.com>
2017-09-11 21:58:35 -0500

    Auth is a vue plugin

--- resources/assets/js/core/auth/index.js
+++ resources/assets/js/core/auth/index.js
@@ -3,89 +3,99 @@ import Vue          from 'vue';
 import { Promise }  from 'es6-promise';
 import { request }  from '@/api';
 
-let user = null
-  , EventHandler = new Vue();
-
-let auth = {
-  events: {
-    loginConfirmed() {
-      EventHandler.$emit('auth:loginConfirmed', user);
-    },
-    userUnauthenticated() {
-      EventHandler.$emit('auth:userUnauthenticated', user);
-    }
-  },
-
-  on: {
-    loginConfirmed: (fn) => {
-      EventHandler.$on('auth:loginConfirmed', fn);
-    },
-    userUnauthenticated: (fn) => {
-      EventHandler.$on('auth:userUnauthenticated', fn);
-    }
-  },
-
-  login(credentials) {
-    return request('auth')
-      .login({}, credentials)
-      .then((response) => {
-        return new Promise((resolve, reject) => {
-          console.log((response));
-          if ('success' === response.data.status) {
-            resolve(auth.user());
-          } else {
-            reject(false);
-          }
-        });
-
-        // return 'success' === response.data.status
-        //   ? auth.user()
-        //   : false; // return rejected promise with false;
-      }, (error) => {
-        return new Promise((resolve, reject) => reject(error));
-      });
-  },
-
-  check() {
-    console.log('checking user');
-    return request('auth')
-      .me()
-      .then((response) => {
-        if ('ok' === response.data.status) {
-          user = response.data.user;
-
-          auth.events.loginConfirmed();
-
-          return user;
+let EventHandler = new Vue();
+let AuthComponent = new Vue({
+  data() {
+    return {
+      user: null,
+      events: {
+        loginConfirmed: () => {
+          EventHandler.$emit('auth:loginConfirmed', this.user);
+        },
+        userUnauthenticated: () => {
+          EventHandler.$emit('auth:userUnauthenticated', this.user);
         }
-
-        return false;
-      }, (error) => {
-        return new Promise((resolve, reject) => reject(error));
-      });
-  },
-
-  user() {
-    if (user) {
-      return new Promise((resolve, reject) => resolve(user));
+      },
+      on: {
+        loginConfirmed(fn) {
+          EventHandler.$on('auth:loginConfirmed', fn);
+        },
+        userUnauthenticated(fn) {
+          EventHandler.$on('auth:userUnauthenticated', fn);
+        }
+      },
     }
-
-    return auth.check();
   },
 
-  logout() {
-    return request('auth').logout().then((response) => {
-      if ('ok' === response.data.status) {
-        user = null;
+  computed: {
+    isAuthenticated() {
+      return !!this.user;
+    },
+  },
 
-        auth.events.userUnauthenticated();
+  methods: {
+    login(credentials) {
+      return request('auth')
+        .login({}, credentials)
+        .then((response) => {
+          return new Promise((resolve, reject) => {
+            if ('success' === response.data.status) {
+              resolve(this.getUser());
+            } else {
+              reject(false);
+            }
+          });
+
+          // return 'success' === response.data.status
+          //   ? auth.user()
+          //   : false; // return rejected promise with false;
+        }, (error) => {
+          return new Promise((resolve, reject) => reject(error));
+        });
+    },
+
+    check() {
+      return request('auth')
+        .me()
+        .then((response) => {
+          if ('ok' === response.data.status) {
+            this.user = response.data.user;
+
+            this.events.loginConfirmed();
+
+            return this.user;
+          }
+
+          return false;
+        }, (error) => {
+          return new Promise((resolve, reject) => reject(error));
+        });
+    },
+
+    getUser() {
+      if (this.user) {
+        return new Promise((resolve, reject) => resolve(this.user));
       }
 
-      return true;
-    }, (error) => {
+      return this.check();
+    },
 
-    });
+    logout() {
+      return request('auth').logout().then((response) => {
+        if ('ok' === response.data.status) {
+          this.user = null;
+
+          this.events.userUnauthenticated();
+        }
+
+        return true;
+      }, (error) => {
+
+      });
+    }
   }
-}
+});
 
-export default auth;
+export default function Auth() {
+  Vue.prototype.$auth = AuthComponent;
+};
