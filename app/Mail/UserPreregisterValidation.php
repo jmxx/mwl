bfe120fd9b4bed4a6f2fabd54aff861c3fc2ebf3
Juan Manuel Reynoso <yeah.mue@gmail.com>
2017-12-18 21:40:52 -0600

    Add user-email validation mail

---
+++ app/Mail/UserPreregisterValidation.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace MWL\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class UserPreregisterValidation extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $token;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct($token)
+    {
+        $this->token = $token;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->view('emails.users.preregistered');
+    }
+}
---
+++ resources/views/emails/users/preregistered.blade.php
@@ -0,0 +1,6 @@
+<div>
+  <h1>Email validation</h1>
+  <div class="">
+    <a href="{{ url()->full() }}/!#/validate/{{ $token }}">Validate</a>
+  </div>
+</div>
